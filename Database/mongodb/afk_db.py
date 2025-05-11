e2caabf85b1e3f9e0f2e8925448204381d63f8fc
AKASH97700 <akashmis2006@gmail.com>
2025-05-11 09:32:52 +0530

    Update afk_db.py

--- Database/mongodb/afk_db.py
+++ Database/mongodb/afk_db.py
@@ -1,3 +1,4 @@
+from typing import Tuple, Union
 from Database.mongodb.db import dbname
 
 usersdb = dbname.users
@@ -7,47 +8,42 @@ cleanmode = {}
 
 async def is_cleanmode_on(chat_id: int) -> bool:
     mode = cleanmode.get(chat_id)
-    if not mode:
-        user = await cleandb.find_one({"chat_id": chat_id})
-        if not user:
-            cleanmode[chat_id] = True
-            return True
-        cleanmode[chat_id] = False
-        return False
-    return mode
+    if mode is not None:
+        return mode
+
+    user = await cleandb.find_one({"chat_id": chat_id})
+    cleanmode[chat_id] = not bool(user)
+    return cleanmode[chat_id]
 
 
 async def cleanmode_on(chat_id: int):
     cleanmode[chat_id] = True
-    user = await cleandb.find_one({"chat_id": chat_id})
-    if user:
-        return await cleandb.delete_one({"chat_id": chat_id})
+    if await cleandb.find_one({"chat_id": chat_id}):
+        await cleandb.delete_one({"chat_id": chat_id})
 
 
 async def cleanmode_off(chat_id: int):
     cleanmode[chat_id] = False
-    user = await cleandb.find_one({"chat_id": chat_id})
-    if not user:
-        return await cleandb.insert_one({"chat_id": chat_id})
+    if not await cleandb.find_one({"chat_id": chat_id}):
+        await cleandb.insert_one({"chat_id": chat_id})
 
 
-async def is_afk(user_id: int) -> bool:
+async def is_afk(user_id: int) -> Tuple[bool, Union[str, dict]]:
     user = await usersdb.find_one({"user_id": user_id})
     return (True, user["reason"]) if user else (False, {})
 
 
-async def add_afk(user_id: int, mode):
+async def add_afk(user_id: int, reason: Union[str, dict]):
     await usersdb.update_one(
-        {"user_id": user_id}, {"$set": {"reason": mode}}, upsert=True
+        {"user_id": user_id}, {"$set": {"reason": reason}}, upsert=True
     )
 
 
 async def remove_afk(user_id: int):
-    user = await usersdb.find_one({"user_id": user_id})
-    if user:
-        return await usersdb.delete_one({"user_id": user_id})
+    if await usersdb.find_one({"user_id": user_id}):
+        await usersdb.delete_one({"user_id": user_id})
 
 
 async def get_afk_users() -> list:
-    users = usersdb.find({"user_id": {"$gt": 0}})
-    return list(await users.to_list(length=1000000000)) if users else []
+    cursor = usersdb.find({"user_id": {"$gt": 0}})
+    return await cursor.to_list(length=None)
